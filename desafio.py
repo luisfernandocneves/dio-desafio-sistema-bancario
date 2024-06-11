866445d0142e4997328d6cc41ed7147a74871109
Luis Fernando Coelho Neves <luis.neves@telefonica.com>
2024-06-11 10:49:20 -0300

    Conclusao das funcoes de criar conta e criar usuario

--- desafio.py
+++ desafio.py
@@ -46,12 +46,69 @@ def visualizarExtrato(saldo, /, *, extrato):
     else:
         print ("###### EXTRATO ##########\n" + extrato + f"\nSaldo: R$ {saldo:.2f}")
 
+def validaCpf (cpf, usuarios):
+    if not cpf.isdigit():
+        return False, "CPF Inválido! Digite apenas os números do CPF "
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]    
+    if usuarios_filtrados:
+        return False, "CPF já cadastrado! "
+    else:
+        return True, ""
+
+def criarUsuario (usuarios):    
+    cpf = input ("Informe o CPF do usuário: ")
+    
+    while True:    
+        valido, mensagem = validaCpf(cpf, usuarios)
+        if not valido:
+            cpf = input (mensagem + " ou digite [q] para voltar ao menu anterior: ")
+            if str(cpf) == "q":
+                break       
+        else:
+            nome = input ("Informe o Nome do usuário: ")
+            dataNascimento = input ("Informe a Data de Nascimento do usuário (dd-mm-aaaa): ")
+            endereco = input("Informe o endereço do usuário (logradouro, nro - bairro - cidade/sigla estado): ")
+            
+            usuarios.append({"nome": nome, "dataNascimento": dataNascimento, "cpf": cpf, "endereco": endereco})
+
+            print("Usuário cadastrado com sucesso!")  
+            break  
+
+def listarUsuarios(usuarios):
+    for usuario in usuarios:
+        print (usuario)
+
+def listarContas(contas):
+    for conta in contas:
+        print (conta)
+
+def criarConta (agencia, contas, usuarios):    
+    cpf = input ("Informe o CPF do usuário: ")
+    
+    while True:    
+        usuario = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+        if not usuario:
+            cpf = input ("Usuário não encontrado, digite um CPF válido ou digite [q] para voltar ao menu anterior: ")
+            if str(cpf) == "q":
+                break       
+        else:
+            conta = {"agencia": agencia, "numeroConta": len(contas) + 1, "usuario": usuario}
+            
+            contas.append(conta)
+
+            print("Conta criada com sucesso!")  
+            break  
+
 def menu():
     menu = """
 
         [d] Depositar
         [s] Sacar
         [e] Extrato
+        [u] Cadastrar Usuarios
+        [c] Criar Conta
+        [lu] Listar Usuarios        
+        [lc] Listar Contas
         [q] Sair
 
         => """
@@ -61,6 +118,9 @@ def menu():
     extrato = ""
     numero_saques = 0
     LIMITE_SAQUES = 3
+    usuarios = []
+    contas = []
+    AGENCIA = "0001"
     
     while True:
         opcao = input(menu)
@@ -75,7 +135,14 @@ def menu():
                   
         elif opcao == "e":        
             visualizarExtrato(saldo, extrato=extrato)    
-
+        elif opcao == "u":
+            criarUsuario(usuarios)
+        elif opcao == "lu":
+            listarUsuarios(usuarios)
+        elif opcao == "c":
+            criarConta(AGENCIA, contas, usuarios)
+        elif opcao == "lc":
+            listarContas(contas)
         elif opcao == "q":
             break
 
